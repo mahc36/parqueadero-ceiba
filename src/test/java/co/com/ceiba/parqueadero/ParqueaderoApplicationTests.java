fe7a56fc84157df20878bb7d04b185f46c9fa0a0
mahc36 <matteohenao37@hotmail.com>
2018-05-16 07:06:34 -0500

    Prueba funcionando

--- src/test/java/co/com/ceiba/parqueadero/ParqueaderoApplicationTests.java
+++ src/test/java/co/com/ceiba/parqueadero/ParqueaderoApplicationTests.java
@@ -1,5 +1,6 @@
 package co.com.ceiba.parqueadero;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -7,5 +8,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @SpringBootTest(classes= ParqueaderoApplication.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 public class ParqueaderoApplicationTests {
-
+	
+	@Test
+	public void init() {
+		
+	}
 }
